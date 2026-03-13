6940ee6339d045996c28b521b03f61b77838c49a
0828378 <163357968+0828378@users.noreply.github.com>
2026-03-13 18:32:48 -0300

    Resoluçao das questoes em Python, estoque de eletrônicos e analisador de texto
    Este commit adiciona a resolução de duas questoes em Python.
    
    O primeiro dever simula o estoque de uma loja de eletrônicos utilizando uma lista de dicionários. O programa calcula o valor total do estoque, identifica itens com valor acima de R$500 e lista produtos que falta.
    
    O segundo dever implementa um analisador de texto que recebe uma frase do usuário, conta a frequência das palavras, palavras repetidas e me da um total de palavras, palavras únicas e a palavra mais usada.

---
+++ atividade2.py
@@ -0,0 +1,58 @@
+
+# DESAFIO 1-ESTOQUE DE ELETRÔNICOS
+estoque = [
+    {"nome": "Notebook", "preco": 3500, "quantidade": 3},
+    {"nome": "Mouse", "preco": 80, "quantidade": 10},
+    {"nome": "Teclado", "preco": 200, "quantidade": 5},
+    {"nome": "Monitor", "preco": 900, "quantidade": 2},
+    {"nome": "Headset", "preco": 450, "quantidade": 0}
+]
+
+valor_total = 0
+
+for item in estoque:
+    valor_item = item["preco"] * item["quantidade"]
+    valor_total += valor_item
+
+    if valor_item > 500:
+        print("Item com valor acima de R$500:", item["nome"])
+
+print("Valor total em estoque: R$", valor_total)
+
+#produtos em falta
+produtos_em_falta = [item["nome"] for item in estoque if item["quantidade"] == 0]
+
+print("Produtos em falta:", produtos_em_falta)
+
+
+# DESAFIO 2-ANALISADOR DE TEXTO
+
+frase = input("\nDigite uma frase: ")
+palavras = frase.lower().split()
+contagem = {}
+
+# Conta as palavras
+for palavra in palavras:
+    if palavra in contagem:
+        contagem[palavra] += 1
+    else:
+        contagem[palavra] = 1
+
+# Total
+total_palavras = len(palavras)
+
+#  palavras únicas
+palavras_unicas = len(contagem)
+
+#mais frequente
+palavra_mais_frequente = max(contagem, key=contagem.get)
+
+#  repetidas
+repetidas = [palavra for palavra, qtd in contagem.items() if qtd > 1]
+
+# final
+print("\n===== RELATÓRIO =====")
+print("Total de palavras:", total_palavras)
+print("Total de palavras únicas:", palavras_unicas)
+print("Palavras repetidas:", repetidas)
+print("Palavra mais frequente:", palavra_mais_frequente)
